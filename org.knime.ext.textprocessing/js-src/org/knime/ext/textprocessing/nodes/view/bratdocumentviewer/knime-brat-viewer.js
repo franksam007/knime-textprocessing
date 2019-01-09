b935b633da5c2acac72aa530aa9cc680c26a4bce
Andisa Dewi <andisa.dewi@icloud.com>
2019-01-09 13:36:10 +0100

    AP-9486: Removed webfont js lib
    AP-9486 (Text Mining: Brat Document Viewer)

--- org.knime.ext.textprocessing/js-src/org/knime/ext/textprocessing/nodes/view/bratdocumentviewer/knime-brat-viewer.js
+++ org.knime.ext.textprocessing/js-src/org/knime/ext/textprocessing/nodes/view/bratdocumentviewer/knime-brat-viewer.js
@@ -1,28 +1,41 @@
 brat_doc_viewer = function() {
 
 	view = {};
+	var _representation = null;
+	var VIZ_ID = 'viz';
+	var minWidth = 100;
+	var minHeight = 100;
+	var layoutContainerID = "layoutContainer";
+	var containerID = "documentContainer";
 
 	view.init = function(representation, value){
 		try{
-			if (!representation.documentText && !representation.terms) {
-				document.getElementsByTagName('body')[0].innerHTML = "<h1>No data available</h1>";
+			_representation = representation;
+
+			if (!_representation.documentText && !_representation.terms) {
+				d3.select("body").append("p").text("Error: No data available");
 				return;
 			}
-			var tags = representation.tags;
-			var title = representation.documentTitle;
-			var text = representation.documentText;
-			var ids = representation.termIds;
-			var terms = representation.terms;
-			var startIdx = representation.startIndexes;
-			var stopIdx = representation.stopIndexes;
-		
+
+			d3.select("html").style("width", "100%").style("height", "100%");
+			var body = d3.select("body").style("width", "100%").style("height", "100%");
+
+			var tags = _representation.tags;
+			var title = _representation.documentTitle;
+			var text = _representation.documentText;
+			var ids = _representation.termIds;
+			var terms = _representation.terms;
+			var startIdx = _representation.startIndexes;
+			var stopIdx = _representation.stopIndexes;
+			var colors = _representation.colors;
+			
 			var tagsUnique = tags.filter(function(item, i, ar){ return ar.indexOf(item) === i; });
 			var collData = {"entity_types" : []};		
 			for (var i = 0; i < tagsUnique.length; i++) {
 				var obj = {};
 				obj.type = tagsUnique[i];
 				obj.labels = [tagsUnique[i]];
-				obj.bgColor =  getRandomColor(hashCode(obj.type));
+				obj.bgColor = colors[i];
 				obj.borderColor = 'darken';
 				collData.entity_types.push(obj);
 			}
@@ -35,18 +48,18 @@ brat_doc_viewer = function() {
 				docData.entities.push(obj);
 			}
 
+			var prefixTitle = 'Document Title: '
 			if(!title){
 				title = 'Untitled';			
 			}
-		        var body = document.getElementsByTagName('body')[0];
-			var html = "<h1>" + title + "</h1>";
-			body.innerHTML = html;
-				
-			var div = document.createElement('div');
-			div.id = 'viz';
-			body.appendChild(div);
-					
-		        Util.embed('viz', collData, docData);
+
+			body.append("h1").text(prefixTitle + title);
+
+		    Util.embed(VIZ_ID, $.extend({}, collData), $.extend({}, docData));
+
+		    if (parent != undefined && parent.KnimePageLoader != undefined) {
+            	parent.KnimePageLoader.autoResize(window.frameElement.id);
+        	}
 		} catch(err) {
 			if (err.stack) {
 				alert(err.stack);
@@ -54,38 +67,21 @@ brat_doc_viewer = function() {
 				alert (err);
 			}
 		}
-		if (parent != undefined && parent.KnimePageLoader != undefined) {
-			parent.KnimePageLoader.autoResize(window.frameElement.id);
-		}
-	}
-
-	function getRandomColor(seed) {
-		/*var letters = '0123456789ABCDEF';
-		var color = '#';
-		for (var i = 0; i < 6; i++) {
-			color += letters[Math.floor(Math.random() * 16)];
-	  	}*/
-		color = Math.floor((Math.abs(Math.sin(seed) * 16777215)) % 16777215);
-    		color = color.toString(16);
-		// add any colors shorter than 6 characters with leading 0s
-		while(color.length < 6) {
-			color = '0' + color;
-		}
-		color = '#' + color;
-	  	return color;
-	}
-
-	/* taken from https://gist.github.com/hyamamoto/fd435505d29ebfa3d9716fd2be8d42f0 */
-	function hashCode(s) {
-	  var h = 0, l = s.length, i = 0;
-	  if ( l > 0 )
-	    while (i < l)
-	      h = (h << 5) - h + s.charCodeAt(i++) | 0;
-	  return h;
 	}
 
 	view.validate = function() {
 		return true;
+	}
+
+	view.getComponentValue = function() {
+		return null;
+	}
+
+	view.getSVG = function() {
+		//var svgElement = document.getElementById("svgID");
+		//var svgElement = d3.select("svg")[0][0];
+		//knimeService.inlineSvgStyles(svgElement);
+		//return (new XMLSerializer()).serializeToString(svgElement);
 	}
 
 	return view;
--- org.knime.ext.textprocessing/lib/js-lib/brat_1.3/client/lib/webfont_readme.txt
+++
@@ -1,3 +0,0 @@
-This plug-in makes use of the Webfont.js library, which is released by: https://github.com/typekit/webfontloader
-
-The library is released under Apache 2.0, which can be found here https://github.com/typekit/webfontloader/blob/master/LICENSE and also in the LICENSE.txt file in this directory.
